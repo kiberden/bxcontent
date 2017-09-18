a3fe4f65db89d98c9cd5063a4739974a2874b0a3
Виталий Акмашев <va@crtweb.ru>
2017-09-18 16:29:43 +0500

    add marvin255.bxcontent collectSnippets event

--- marvin255.bxcontent/include.php
+++ marvin255.bxcontent/include.php
@@ -1,3 +1,22 @@
 <?php
 
+use marvin255\bxcontent\SnippetManager;
+use marvin255\bxcontent\Exception;
+use Bitrix\Main\Event;
+
 require_once __DIR__ . '/lib/Autoloader.php';
+
+$snippetManager = SnippetManager::getInstance(true);
+
+$event = new Event(
+    'marvin255.bxcontent',
+    'collectSnippets',
+    ['snippetManager' => $snippetManager]
+);
+$event->send();
+foreach ($event->getResults() as $eventResult)
+{
+    if ($eventResult->getType() === EventResult::ERROR) {
+        throw new Exception('Get error while collecting snippets: ' . implode(',', $eventResult->getErrorMessages()));
+    }
+}
