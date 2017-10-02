8e42e7cf45e2b0b70f5b73f0a3e42c67ce518afa
Виталий Акмашев <va@crtweb.ru>
2017-10-02 16:24:35 +0500

    add internal rendering to tabs snippet

--- marvin255.bxcontent/lib/packs/bootstrap/Tabs.php
+++ marvin255.bxcontent/lib/packs/bootstrap/Tabs.php
@@ -15,6 +15,13 @@ use marvin255\bxcontent\controls\Combine;
 class Tabs extends Pack
 {
     /**
+     * Счетчик для получения уникальных идентификаторов слайдеров.
+     *
+     * @var int
+     */
+    protected static $idCounter = 0;
+
+    /**
      * @inheritdoc
      */
     protected function getDefaultSettings()
@@ -27,12 +34,12 @@ class Tabs extends Pack
         ];
 
         $return['controls'][] = new Combine([
-            'name' => 'tabs',
+            'name' => 'items',
             'label' => 'Табы',
             'multiple' => true,
             'elements' => [
                 new Input(['name' => 'caption', 'label' => 'Заголовок']),
-                new Editor(['name' => 'content', 'label' => 'Содержимое']),
+                new Editor(['name' => 'text', 'label' => 'Содержимое']),
             ],
         ]);
 
@@ -52,5 +59,44 @@ class Tabs extends Pack
      */
     protected function renderInternal(array $snippetValues)
     {
+        $return = '';
+        if (!empty($snippetValues['items']) && is_array($snippetValues['items'])) {
+            $id = 'bootstrap-tabs-' . static::$idCounter;
+            ++static::$idCounter;
+
+            $key = 0;
+            $menu = '';
+            $items = '';
+            foreach ($snippetValues['items'] as $item) {
+                if (empty($item['caption'])) {
+                    continue;
+                }
+
+                $menu .= '<li role="presentation"' . ($key === 0 ? ' class="active"' : '') . '>';
+                $menu .= '<a href="#' . $id . '-' . $key . '" aria-controls="home" role="tab" data-toggle="tab">';
+                $menu .= htmlentities($item['caption']);
+                $menu .= '</a>';
+                $menu .= '</li>';
+
+                $items .= '<div role="tabpanel" class="tab-pane' . ($key === 0 ? ' active' : '') . '" id="' . $id . '-' . $key . '">';
+                $items .= isset($item['text']) ? $item['text'] : '';
+                $items .= '</div>';
+
+                ++$key;
+            }
+
+            if ($items) {
+                $return .= '<div>';
+                $return .= '<ul class="nav nav-tabs" role="tablist">';
+                $return .= $menu;
+                $return .= '</ul>';
+                $return .= '<div class="tab-content">';
+                $return .= $items;
+                $return .= '</div>';
+                $return .= '</div>';
+            }
+        }
+
+        return $return;
     }
 }
