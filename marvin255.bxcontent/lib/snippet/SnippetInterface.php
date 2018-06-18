38daf8efc26a54ed67ef72e441fa6ba8a3043a90
marvin255 <postbox433@gmail.com>
2018-06-18 19:14:24 +0500

    fix comments to file

--- marvin255.bxcontent/lib/snippet/SnippetInterface.php
+++ marvin255.bxcontent/lib/snippet/SnippetInterface.php
@@ -7,18 +7,24 @@ use JsonSerializable;
 /**
  * Интерфейс для объекта сниппета, который содержит в себе настройки для
  * отображения управляющего элемента для ввода данных.
+ *
+ * Ключевые параметры сниппета - имя и тип. Имя должно быть уникальным
+ * в рамках контекста (сниппет может быть вложен в другой сниппет, значит в рамках родителя
+ * или же в списке сниппетов, который будут отправлены для отображения).
+ * Тип может быть общий для нескольких сниппетов и используется для того, чтобы
+ * объединить сниппеты с одинаковой логикой для представления.
  */
 interface SnippetInterface extends JsonSerializable
 {
     /**
-     * Возвращает тип указанного сниппета.
+     * Возвращает тип данного сниппета.
      *
      * @return string
      */
     public function getType();
 
     /**
-     * Задает родительский сниппет для текущего сниппета.
+     * Задает родительский сниппет для данного сниппета.
      *
      * @param \marvin255\bxcontent\snippet\SnippetInterface $parentSnippet
      *
@@ -27,14 +33,14 @@ interface SnippetInterface extends JsonSerializable
     public function setParent(SnippetInterface $parentSnippet);
 
     /**
-     * Возвращает родительский сниппет для текущего сниппета или null если родителя нет.
+     * Возвращает родительский сниппет для данного сниппета или null если родителя нет.
      *
      * @return \marvin255\bxcontent\snippet\SnippetInterface|null
      */
     public function getParent();
 
     /**
-     * Возвращает машиночитаемое название текущего сниппета.
+     * Возвращает уникальное в рамках контекста имя данного сниппета.
      *
      * @return string
      */
@@ -48,7 +54,7 @@ interface SnippetInterface extends JsonSerializable
     public function getInputName();
 
     /**
-     * Задает значение данного объекта сниппета.
+     * Задает значение данного сниппета.
      *
      * @param mixed $value
      *
@@ -57,7 +63,7 @@ interface SnippetInterface extends JsonSerializable
     public function setValue($value);
 
     /**
-     * Возвращает значение данного объекта сниппета.
+     * Возвращает значение данного сниппета.
      *
      * @return mixed
      */
